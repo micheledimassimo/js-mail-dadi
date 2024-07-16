77377a9971a3f004b47913b1ed87db624aee7c4f
micheledimassimo <mcldms@outlook.it>
2024-07-16 17:01:17 +0200

    v1.1 finito dadi

---
+++ DADI/js/scripts.js
@@ -0,0 +1,47 @@
+console.log('JS collegato');
+
+const fistNumDiv = document.querySelector('div#first-num')
+//faccio generare il PRIMO numero random
+let numbOne = document.getElementById('first-num');
+numbOne = Math.floor(Math.random() * 6) + 1;
+console.log('Primo numero', numbOne)
+
+//creo un nuovo div 
+let newDiv = document.createElement('div');
+
+//do le classi al div
+newDiv.classList.add("my-col");
+newDiv.classList.add("dice");
+
+//aggiungo elemento
+newDiv.innerHTML = numbOne;
+fistNumDiv.append(newDiv);
+
+//SECONDO NUMERO
+const secondNumDiv = document.querySelector('div#second-num')
+//faccio generare il primo numero random
+let numbTwo = document.getElementById('second-num');
+numbTwo = Math.floor(Math.random() * 6) + 1;
+console.log('Secondo numero', numbTwo)
+
+//creo un nuovo div 
+let newDivTwo = document.createElement('div');
+
+//do le classi al div
+newDivTwo.classList.add("my-col");
+newDivTwo.classList.add("dice");
+
+//aggiungo elemento
+newDivTwo.innerHTML = numbTwo;
+secondNumDiv.append(newDivTwo);
+
+//confronto i due numeri generati 
+if (numbOne > numbTwo) {
+    document.getElementById('winner').innerHTML = 'Hai vinto tu!'
+}
+else if (numbOne == numbTwo) {
+    document.getElementById('winner').innerHTML = 'Pareggio'
+}
+else {
+    document.getElementById('winner').innerHTML = 'Hai Perso!'
+}
